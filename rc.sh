0e62d3103755db9f388f0418ec61cb994c4fed09
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-12-22 19:03:42 +0100

    Load completion script for Poetry if available

--- rc.sh
+++ rc.sh
@@ -253,6 +253,20 @@ then
 				) \
 				#
 		fi
+
+		# The Bash completion script for Poetry requires bash-completion
+		if command -v -- poetry &> /dev/null
+		then
+			source \
+				-- \
+				<(
+					poetry \
+						completions \
+						bash \
+						#
+				) \
+				#
+		fi
 	fi
 
 
