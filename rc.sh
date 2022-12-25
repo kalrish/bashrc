936bd1d95c04683fbf2c7649510e2c26ff1483c8
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-12-25 22:29:37 +0100

    Do not load completion script for Poetry
    This reverts commit 0e62d3103755db9f388f0418ec61cb994c4fed09.

--- rc.sh
+++ rc.sh
@@ -253,20 +253,6 @@ then
 				) \
 				#
 		fi
-
-		# The Bash completion script for Poetry requires bash-completion
-		if command -v -- poetry &> /dev/null
-		then
-			source \
-				-- \
-				<(
-					poetry \
-						completions \
-						bash \
-						#
-				) \
-				#
-		fi
 	fi
 
 
